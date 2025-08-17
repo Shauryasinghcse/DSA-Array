ed28addf63718bc3e942f86b3e7b01f58ed10e5e
Shauryasinghcse <shauryasinghcse@gmail.com>
2025-08-17 11:51:47 +0530

    Add files via upload

---
+++ inputoutput.java
@@ -0,0 +1,15 @@
+package array;
+
+public class inputoutput {
+    public static void main(String[] args) {
+        int[] arr = {85, 12, 45, 12, 36};
+        int i = 0;
+        while (i <= 4) {
+            System.out.println(arr[i] + "");
+            i++;
+        }
+            System.out.print( "Length of array is" + " ");
+            System.out.println(arr.length);
+
+    }
+}
